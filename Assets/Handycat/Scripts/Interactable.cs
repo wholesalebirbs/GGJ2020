26ffdc4ce724ed2b2f3c63fd5b4f752e2fd036c0
Miguel Torres <torresmiguel@protonmail.ch>
2020-02-01 15:19:17 -0800

    Added breakables

---
+++ Assets/Handycat/Scripts/Interactable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+namespace Buga
+{
+
+    public class Interactable : MonoBehaviour
+    {
+        public static UnityAction<Interactable> OnInteractCalled;
+
+        bool canInteract = true;
+
+        protected virtual void Initialize()
+        {
+
+        }
+
+        public virtual void Interact()
+        {
+            if (!canInteract)
+            {
+                return;
+            }
+
+            OnInteractCalled(this);
+
+        }
+    }
+
+}
