dd380dc330a2d9b0a77e880ac7208a2b699c3676
Miguel Torres <torresmiguel@protonmail.ch>
2020-02-02 15:57:59 -0800

    Added human and cat audio controllers

---
+++ Assets/Handycat/Scripts/RandomAudioPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buga
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class RandomAudioPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        List<AudioClip> clips = new List<AudioClip>();
+
+        AudioSource source;
+
+        protected virtual void Awake()
+        {
+            source = GetComponent<AudioSource>();
+        }
+
+        protected virtual void PlayRandomSound()
+        {
+            if (!source.isPlaying)
+            {
+                source.clip = clips[Random.Range(0, clips.Count)];
+                source.Play();
+            }
+        }
+    }
+
+}
