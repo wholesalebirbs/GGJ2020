b35576a7ae0989c2a096d62c9aa620c1a076002d
Miguel Torres <torresmiguel@protonmail.ch>
2020-02-02 13:37:27 -0800

    Added RockBand

---
+++ Assets/Handycat/Scripts/TileDetector.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Buga
+{
+
+    public enum  TileType
+    {
+        Red,
+        Yellow,
+        Blue,
+        Green
+    }
+
+    public class TileDetector : MonoBehaviour
+    {
+        [SerializeField]
+        protected float maxDetectiondistance = 1;
+
+        [SerializeField]
+        protected string triggerDetectionButton;
+        [SerializeField]
+        protected Transform tilesTarget;
+        public Transform TilesTarget
+        {
+            get => tilesTarget;
+        }
+
+        public List<MovingObject> currentTiles;
+
+
+        public bool HasTiles
+        {
+            get
+            {
+
+                Debug.Log($"Current tiles on {gameObject.name}: {currentTiles.Count}");
+                return currentTiles.Count > 0;
+            }
+        }
+
+        public static UnityAction OnTileMissed;
+
+        public static UnityAction OnTileHit;
+
+        public bool acceptingInput = false;
+        public void ToggleInput(bool status)
+        {
+            acceptingInput = status;
+        }
+
+        private void OnEnable()
+        {
+            MovingObject.OnTargetReached += OnMovingObjectTargetReached;
+        }
+        private void OnDisable()
+        {
+            MovingObject.OnTargetReached -= OnMovingObjectTargetReached;
+        }
+
+        private void Update()
+        {
+            if (acceptingInput)
+            {
+                if (Input.GetButtonDown(triggerDetectionButton))
+                {
+                    Debug.Log($"{gameObject.name} detecting Input");
+                    Detect();
+                }
+            }
+        }
+
+        protected void OnMovingObjectTargetReached(MovingObject m)
+        {
+            for (int i = 0; i < currentTiles.Count; i++)
+            {
+                if (currentTiles[i] == m)
+                {
+                    GameObject tile = currentTiles[i].gameObject;
+                    currentTiles.RemoveAt(i);
+
+                    Destroy(tile);
+
+                    OnTileMissed?.Invoke();
+                }
+            }
+        }
+
+        public void Detect()
+        {
+            for (int i = 0; i < currentTiles.Count; i++)
+            {
+                if (Vector3.Distance(currentTiles[i].transform.position, transform.position) <= maxDetectiondistance)
+                {
+                    GameObject tile = currentTiles[i].gameObject;
+                    currentTiles.RemoveAt(i);
+
+                    Destroy(tile);
+
+                    Debug.Log("Tile Hit!");
+                    OnTileHit?.Invoke();
+                    return;
+                }
+            }
+
+            Debug.Log("Tile Missed");
+            OnTileMissed?.Invoke();
+        }
+
+        public void CleanUp()
+        {
+            acceptingInput = false;
+
+            for (int i = currentTiles.Count -1; i > -1; i--)
+            {
+                Destroy(currentTiles[i].gameObject);
+            }
+
+            currentTiles.Clear();
+        }
+
+
+        public void AddTile(MovingObject mo)
+        {
+            currentTiles.Add(mo);
+        }
+
+    }
+}
+
---
+++ Assets/Handycat/Scripts/TileSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Buga
+{
+    public class TileSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        protected TileDetector detector;
+
+        [SerializeField]
+        protected GameObject tilePrefab;
+
+
+        protected float minSpawnRate = 1.0f;
+        protected float maxSpawnRate = 5.0f;
+
+
+        bool running = false;
+        float nextSpawn = 0;
+
+        public static UnityAction OnTileSpawned;
+
+        public void BeginSpawning()
+        {
+            nextSpawn = Time.time + Random.Range(minSpawnRate, maxSpawnRate);
+            running = true;
+        }
+
+        public void StopSpawning()
+        {
+            running = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (running == true)
+            {
+                if (Time.time > nextSpawn)
+                {
+                    Spawn();
+                }
+            }
+           
+        }
+
+
+        protected void Spawn()
+        {
+            GameObject go = Instantiate(tilePrefab, transform.position, Quaternion.identity, this.transform);
+            go.GetComponent<MovingObject>().SetTarget(detector.TilesTarget);
+
+            detector.AddTile(go.GetComponent<MovingObject>());
+
+            nextSpawn = Time.time + Random.Range(minSpawnRate, maxSpawnRate);
+
+            Debug.Log($"{gameObject.name} spawned tile");
+            OnTileSpawned?.Invoke();
+        }
+    }
+}
