ad6206abfbd8da55bdb6ab5793ee6da50979ebcc
Miguel Torres <torresmiguel@protonmail.ch>
2020-02-01 17:45:17 -0800

    Added Human Interaction

---
+++ Assets/Handycat/Scripts/Interactor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buga
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class Interactor : MonoBehaviour
+    {
+        private bool canInteract = false;
+
+        private Interactable activeInteractable;
+
+        public void InteractWithInteractable()
+        {
+            if (activeInteractable != null && canInteract)
+            {
+                Debug.Log($"{name} interacting with {activeInteractable.gameObject.name}");
+                activeInteractable.Interact();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Interactable temp = other.GetComponent<Interactable>();
+            if (temp != null)
+            {
+
+                Debug.Log($"Triggered by: {other.name}");
+                activeInteractable = temp;
+                canInteract = true;
+            }
+        }
+
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.GetComponent<Interactable>() == activeInteractable)
+            {
+                canInteract = false;
+                activeInteractable = null;
+            }
+        }
+    }
+}
---
+++ Assets/Handycat/Scripts/MinigameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buga
+{
+    public class MinigameController : MonoBehaviour
+    {
+        [SerializeField]
+        protected List<Minigame> minigames = new List<Minigame>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
